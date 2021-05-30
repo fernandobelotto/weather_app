062c4a86169997a7aac0e74ae7533c42454188c4
Fernando Belotto <fernando.bbosco@gmail.com>
2021-05-30 10:10:17 -0300

    Initial Commit

---
+++ weather_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather_app
+//
+//  Created by Fernando Gabriel Belotto Bosco on 30/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Fernando Gabriel Belotto Bosco on 30/05/21.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
